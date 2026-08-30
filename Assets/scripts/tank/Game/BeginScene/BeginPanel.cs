15069f59ff89e1b8664b8c301e4f40a8f4e979ba
15303782041 <15303782041@163.com>
2026-08-31 02:26:54 +0800

    fix: BeginPanel补UnityEngine.UI命名空间（联机入口CanvasScaler CS0246编译错误）

--- Assets/scripts/tank/Game/BeginScene/BeginPanel.cs
+++ Assets/scripts/tank/Game/BeginScene/BeginPanel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using GameFramework;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class BeginPanel : BasePanel<BeginPanel>
 {
